ecfb171e232b9d1af9190c1e3dd113163c9a1d99
onlinekkabchi <onlinekkabchi@gmail.com>
2022-07-29 16:48:09 +0900

    test: init function 활성 테스트

--- hosting/files/init.js
+++ hosting/files/init.js
@@ -3,12 +3,12 @@ function createStoryBox(el) {
 }
 
 function init() {
-    const response = async () =>
-        await fetch("http://localhost:3000/")
-            .then((res) => console.log(res))
-            .catch((error) => console.error("Error", error));
+    // const response = () =>
+    //     fetch("http://localhost:3000/")
+    //         .then((res) => console.log(res))
+    //         .catch((error) => console.error("Error", error));
 
-    return response;
+    console.log("init");
 }
 
 export { init };
